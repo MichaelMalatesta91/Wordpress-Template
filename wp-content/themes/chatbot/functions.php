8218f364b3e486bb7b22b49d76ed088974310a33
MichaelMalatesta91 <159482698+MichaelMalatesta91@users.noreply.github.com>
2025-03-21 17:48:54 +0000

    Attempting to Swap Chatbot to Malatesta Branch instead of Main

--- wp-content/themes/chatbot/functions.php
+++ wp-content/themes/chatbot/functions.php
@@ -11,7 +11,7 @@ function theme_enqueue_styles() {
         array('parent-style')
     );
     wp_enqueue_script('custom_javascript', get_stylesheet_directory_uri() . '/customElements.js');
-    wp_enqueue_script_module('custom_module1', "https://michaelmalatesta91.github.io/Assignment-2/chat.js");
+    wp_enqueue_script_module('custom_module1', "https://github.com/MichaelMalatesta91/Assignment-2/blob/malatesta/chat.js");
     wp_enqueue_script_module('custom_module2', "https://rhildred.github.io/chatter/fab.js");
 }
 //
